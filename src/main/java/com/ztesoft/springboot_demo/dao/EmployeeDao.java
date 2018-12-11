d1bf7c3de8edb1904db967fbfa552ab70f8ad38d
st <993047454@qq.com>
2018-12-11 10:26:05 +0800

    无效jar包去除

--- src/main/java/com/ztesoft/springboot_demo/dao/EmployeeDao.java
+++ src/main/java/com/ztesoft/springboot_demo/dao/EmployeeDao.java
@@ -1,8 +1,6 @@
 package com.ztesoft.springboot_demo.dao;
 
 import com.ztesoft.springboot_demo.entity.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 public interface EmployeeDao {
